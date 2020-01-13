8500efd59aeb0ce004d33e50b8ac254a8271abff
Mirek Kratochvil <exa.exa@gmail.com>
2020-01-13 21:37:33 +0100

    bump version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 How to build and push:
 
 1. Write commit ID(s) to `scripts/install-shinysom.R`
-2. `docker build -t exaexa/shinysom:$(date +%Y%m%d)`
+2. `docker build -t exaexa/shinysom:$(date +%Y%m%d) .`
 3. `docker image tag exaexa/shinysom:$(date +%Y%m%d) exaexa/shinysom:latest`
 4. `docker login ........`
 5. `docker image push exaexa/shinysom:$(date +%Y%m%d)`
--- scripts/install-shinysom.R
+++ scripts/install-shinysom.R
@@ -2,7 +2,7 @@ devtools::install_github('tsieger/mhca')
 devtools::install_github('exaexa/EmbedSOM')
 devtools::install_github('exaexa/scattermore')
 devtools::install_github('exaexa/shinyDendro')
-devtools::install_gitlab('exaexa/ShinySOM', ref="28174b34d443f1c2744781195420fae25281c5f1")
+devtools::install_gitlab('exaexa/ShinySOM', ref="8b3c6568c2cd363a2add1b88254db32aab066b00")
 
 setwd('/home/shinysom')
 dir.create('data')
